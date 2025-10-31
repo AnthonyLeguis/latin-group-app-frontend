4db1a6fe158b3031ab1bf408b8f1ef8efc818f39
AnthonyLeguis <anthony.leguisamo.g@outlook.com>
2025-10-31 01:21:49 -0400

    feat: agregar configuraciones de producción y reemplazo de archivos en angular.json

---
+++ src/app/core/config/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://latingroupcorp.com/api/v1',
+    appName: 'LatinGroup App',
+    version: '1.0.0',
+    recaptchaSiteKey: '6Lf7yvwrAAAAACvAfL4pcBxcf_hvZQVHNOvfV4b5'
+};
